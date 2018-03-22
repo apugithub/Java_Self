a542043a4e87e2fc012e67a9733a75d6d86ac648
apugithub <apugithub@users.noreply.github.com>
2018-03-22 22:31:02 +0530

    editted Week2Ch2

--- Assignment/Week2Challange2.java
+++ Assignment/Week2Challange2.java
@@ -1,69 +1,114 @@
-/////////////  test encrypt /////////////////
 
-import java.util.Scanner;
-
-
-public class test {
-
-	public static void main(String[] args) {
-		
-		Scanner bucky = new Scanner(System.in);
-		String s = bucky.nextLine();
-		test cls = new test();  
-		cls.encrypt(s);  //you can't make static reference to non-static method, thats why had to create a class instance.
-		System.out.println("llsd "+ s.length());
-	}
-      public  String encrypt(String a){
-		
-		for(int i=0; i<a.length(); i++){
-			char c = a.charAt(i);
-			int j = c;
-			System.out.println("The ascii value of "+ c+ " is "+ j);
-		}
-		return "a";
-     }
-      
- }
- 
- 
- ///////////////////   main ////////////////////////
- 
- 
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 interface BankTransfers{
-	//public String encrypt(String a);
-	//public String decrypt(String a);
+	public String encrypt(String a);
+	public String decrypt(String a);
 }
 
  class ICICI implements BankTransfers {
 	
-	 //public String encrypt(String a)
-	 //public String decrypt(String a);
+	 public String encrypt(String a){
+		 char arr[] =  new char[a.length()];
+			for(int i=0; i<a.length(); i++){
+				char c = a.charAt(i);
+				int j = c + 1;  // j retains the ASCII number of c and (need to increase ASCII value by 1 as per requirement)
+				char k = (char) j;  ///This converts the integer j into its corresponding ASCII value and being assigned as char. 
+				arr[i]=k;
+			}
+
+		//System.out.println(Arrays.toString(arr));  //This Arrays.toString() actually prints all the array contents in a single line
+		String str = Arrays.toString(arr)
+		.replace(", ", "1").replace("[", "").replace("]", "");  //note the replace method and at first "comma and one space" getting replaced by null
+		System.out.println("The Encrypted message is : " +str);
+				
+			return "a";
+	 }
+	 public String decrypt(String a){
+		 System.out.println("The Decrypted message is : "+a);
+		 return "a";
+	 }
 	
 }
 
  class HDFC implements BankTransfers{
 	 
+	 public  String encrypt(String a){
+			
+   	  char arr[] =  new char[a.length()];
+		for(int i=0; i<a.length(); i++){
+			
+			char c = a.charAt(i);
+									
+		if(c!= ' ') {            ///This if loop is required to bypass the space characters as per requirement
+			
+			if (i%2==0){
+
+				int j = c + 1;  // This is as per requirement (need to increase ASCII value by 1)
+				char k = (char) j;  //This converts the integer j into its corresponding ASCII value
+				arr[i] = k; 
+			       }
+			
+			else {
+				int m = c-1;
+				char n = (char) m;
+				arr[i] = n;
+			    }
+		
+			} 
+		}
+		
+			//System.out.println(Arrays.toString(arr));  //This actually prints all the array contents in a single line
+			String str = Arrays.toString(arr)
+					.replace(", ", "").replace("[", "").replace("]", "").replace(" ", " ");
+			System.out.println(str);
+			
+		return str;
+    }
+	 
+	 
+	 public String decrypt(String a){
+		 System.out.println("The Decrypted message is : "+a);
+		 return "a";
+	 }
+	
+	 
  }
-public class week2challange2 {
+public class week2ch2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Scanner bucky = new Scanner(System.in);
-		//System.out.println("Select the Bank Name");
-		//String bnk = bucky.next();
 		
-					
+		Scanner bucky = new Scanner(System.in);
+		System.out.println("Select the Bank Name"+ "\n"+ "1.ICICI"+"\n"+ "2.HDFC");
+		int bnk = bucky.nextInt();
+		//bucky.nextLine();
+		//System.out.println("Enter the Message to Transfer");
+		//String str = bucky.nextLine();
 		
 		ICICI ic = new ICICI();
 		HDFC hd = new HDFC();
-		System.out.println("Enter the Message to Transfer");
+		
+				
+		if (bnk ==1){
+			System.out.println("Enter the Message to Transfer");
+			String str = bucky.nextLine();
+			bucky.nextLine();
+			ic.encrypt(str);
+			ic.decrypt(str);
+		}
+		
+		if (bnk==2){
+			System.out.println("Enter the Message to Transfer");
+			String str = bucky.nextLine();
+			bucky.nextLine();
+			hd.encrypt(str);
+			hd.decrypt(str);
+		}
+		
+		if (bnk!=1 || bnk!=2)
+			System.out.println("Invalid Type");
 
 	}
-
-	
-	
 	
 }
