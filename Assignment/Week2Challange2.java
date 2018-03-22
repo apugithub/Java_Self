4cf6305418695cc4f79a111b0c7c658ba5f5080c
apugithub <sgupdate1988@gmail.com>
2018-03-22 11:51:15 +0530

    wk2ch1 encrypt in progress

--- Assignment/Week2Challange2.java
+++ Assignment/Week2Challange2.java
@@ -1,8 +1,36 @@
-
+/////////////  test encrypt /////////////////
 
 import java.util.Scanner;
 
-import org.omg.Messaging.SyncScopeHelper;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		Scanner bucky = new Scanner(System.in);
+		String s = bucky.nextLine();
+		test cls = new test();  
+		cls.encrypt(s);  //you can't make static reference to non-static method, thats why had to create a class instance.
+		System.out.println("llsd "+ s.length());
+	}
+      public  String encrypt(String a){
+		
+		for(int i=0; i<a.length(); i++){
+			char c = a.charAt(i);
+			int j = c;
+			System.out.println("The ascii value of "+ c+ " is "+ j);
+		}
+		return "a";
+     }
+      
+ }
+ 
+ 
+ ///////////////////   main ////////////////////////
+ 
+ 
+
+import java.util.Scanner;
 
 interface BankTransfers{
 	//public String encrypt(String a);
@@ -11,22 +39,23 @@ interface BankTransfers{
 
  class ICICI implements BankTransfers {
 	
-	 //public String encrypt(String a){
-		 
-	 
+	 //public String encrypt(String a)
+	 //public String decrypt(String a);
 	
 }
 
  class HDFC implements BankTransfers{
 	 
  }
-public class wee2ch2 {
+public class week2challange2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner bucky = new Scanner(System.in);
-		System.out.println("Select the Bank Name");
-		String bnk = bucky.next();
+		//Scanner bucky = new Scanner(System.in);
+		//System.out.println("Select the Bank Name");
+		//String bnk = bucky.next();
+		
+					
 		
 		ICICI ic = new ICICI();
 		HDFC hd = new HDFC();
@@ -34,4 +63,7 @@ public class wee2ch2 {
 
 	}
 
+	
+	
+	
 }
